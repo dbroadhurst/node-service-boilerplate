b725d4deeddac7bc966f4ad31191e9fe50c96f1c
dbroadhurst <david@union25.com>
2020-11-17 11:27:28 -0500

    added jest

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## ESLint standard
 
-https://github.com/standard/eslint-config-standard-with-typescript
+<https://github.com/standard/eslint-config-standard-with-typescript>
+
+## Jest Setup
+
+<https://basarat.gitbook.io/typescript/intro-1/jest#step-1-install>
--- src/server.ts
+++ src/server.ts
@@ -5,37 +5,17 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 
 const PORT = process.env.PORT ?? 3000
-const CORS = process.env.CORS ?? '["http://localhost:3000"]'
-const CSP = process.env.CSP ?? '["http://localhost:3000"]'
 
 const morganType =
   process.env.NODE_ENV === 'production' ? ':date[iso] :remote-addr :method :url :status :response-time ms - :res[content-length]' : 'dev'
 
 export default function start() {
   const server = express()
-    .set('trust proxy', true)
-    .use(cors({ origin: CORS, credentials: true }))
-    .use(
-      helmet({
-        contentSecurityPolicy: {
-          directives: {
-            defaultSrc: CSP,
-            styleSrc: ["'self'", "'unsafe-inline'"],
-            frameAncestors: CSP,
-          },
-        },
-        frameguard: false,
-        hsts: {
-          maxAge: 63072000,
-          includeSubDomains: true,
-          preload: true,
-        },
-      })
-    )
+    .use(cors())
+    .use(helmet())
     .use(morgan(morganType))
     .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.json())
-    .use(bodyParser.json({ type: 'application/csp-report' }))
     .use('/health', (req: Request, res: Response) => res.sendStatus(200))
 
   server.listen(PORT, () => console.info(`Server is running on port ${PORT}`))
